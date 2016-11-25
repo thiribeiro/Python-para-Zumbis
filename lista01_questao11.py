c2dd64f9f2c26fdfd3be19c4171309c5f4be0569
Thiago Ribeiro <ribeiro.thi@gmail.com>
2016-11-25 19:30:30 -0200

    Delete lista01_questao11.py

--- lista01_questao11.py
+++
@@ -1,6 +0,0 @@
-# Questao 11
-# 11) Sabendo que str( ) converte valores numéricos para string, calcule quantos dígitos há em 2 elevado a um milhão.
-
-numero = 2**1000000    			# 2 elevado a um milhão
-numdigit = str(numero)			# Transformando numero em string
-len(numdigit)					# Pegando o tamanho do resultado da string
